fb5f7dc08af4a84a5bbcf55a7606a05982eafd64
Leeiss <farrakhova.2004@mail.ru>
2023-10-16 00:30:05 +0300

    NewtonPolynomial

--- main.cpp
+++ main.cpp
@@ -19,8 +19,52 @@ double Summ(double x) {
     return s;
 }
 
+// Определение функции Si(x) с использованием заданного ряда
+    double Si(double x, double eps = 1e-6) {
+    int n = 0;
+    double term = x;
+    double result = term;
+
+    while (std::fabs(term) >= eps) {
+        n++;
+        term = (-term * x * x) / ((2 * n + 1) * (2 * n + 1));
+        result += term;
+    }
+
+    return result;
+}
+
+// Вычисление коэффициентов разделенных разностей Ньютона
+double f(double x, int n, double xi[]) {
+    if (n == 0) return Si(xi[0]);
+    double result = 0;
+    for (int i = 0; i <= n; i++) {
+        double term = Si(xi[i]);
+        for (int j = 0; j <= n; j++) {
+            if (j != i) {
+                term /= (xi[i] - xi[j]);
+            }
+        }
+        result += term;
+    }
+    return result;
+}
+
+// Построение интерполяционного полинома Ньютона
+double NewtonInterpolation(double x, int n, double xi[]) {
+    double result = f(xi[0], 0, xi);
+    double product = 1;
+    for (int i = 1; i <= n; i++) {
+        product *= (x - xi[i - 1]);
+        result += product * f(xi[i], i, xi);
+    }
+    return result;
+}
+
+
 void Tabulation() {
 /*  
+    Задание 1
     Протабулировать Si(x) на отрезке [a,b] с шагом h с точностью e, основываясь на ряде Тейлора, 
     предварительно вычислив его
     Si(x)=сумма от n=0 до бесконечности ((-1)^n*x^(2n+1))/((2n+1)*(2n+1)!),
@@ -48,8 +92,9 @@ void Tabulation() {
     std::cout << std::endl;
 }
 
-void ConstructionOfNewtonInterpolationPolynomial(){
+void NewtonPolynomial(){
 /*
+    Задание 2
     По полученной таблице значений посторить интерполяционный полином
     Ньютона, приближающий Si(x)
     L_n(x) = f(x_0)+(x-x_o)f(x_0,x_1)+(x-x_0)(x-x_1)f(x_0,x_1,x_2)+...
@@ -58,12 +103,34 @@ void ConstructionOfNewtonInterpolationPolynomial(){
     e_n = max(e(x)) (x∈(a,b)), e(x) = |Si(x)-L_n(x)|
 
 */
+    double a = 0;
+    double b = 4;
+    double h = 0.3;
+    int n = static_cast<int>((b - a) / h);
+    double eps = 1e-6;
+
+    double xi[100]; 
+    for (int i = 0; i <= n; i++) {
+        xi[i] = a + i * h;
+    }
+
+    std::cout << "x_i\t\tSi(x)\t\tL_n(x)\t\t e(x)\n";
+
+    for (int i = 0; i <= n; i++) {
+        double x = xi[i];
+        double Si_x = Si(x);
+        double L_n_x = NewtonInterpolation(x, i, xi);
+        double error = std::abs(Si_x - L_n_x);
+
+        std::cout << std::fixed << std::setprecision(6)
+                  << x << "\t\t" << Si_x << "\t\t" << L_n_x << "\t\t" << error << "\n";
+    }
+
 
 }
 
-
-
 int main() {
     Tabulation();
+    NewtonPolynomial();
     return 0;
 }
